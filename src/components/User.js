767a95a5e0050f8c61771203bcd50a5f2ab69eb9
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-19 11:43:47 -0300

    Adicionada mudança de imagem de perfil

--- src/components/User.js
+++ src/components/User.js
@@ -3,19 +3,29 @@ import { Pencil } from 'react-ionicons'
 
 import "../assets/css/user.css";
 
-const userImage = require("../assets/images/catanacomics.svg");
-
 const User = () => {
     const [userName, setUsername] = useState("Catana");
+    const [userImage, setUserImage] = useState(require("../assets/images/catanacomics.svg").default);
 
     function changeUsername() {
-        let name = prompt("Qual o seu novo (e belo) nome de usuário?");
+        const name = prompt("Qual o seu novo (e belo) nome de usuário?");
         setUsername(name ? name : userName);
+    }
+
+    function changeUserImage() {
+        const image = prompt("Insira sua nova (e bela) imagem de perfil por uma URL:");
+        try {
+            new URL(image);
+            if (image.match(/\.(jpeg|jpg|png)$/) != null) setUserImage(image);
+            else alert("Somente imagens JPG/JPEG e PNG são aceitas.");
+        } catch (error) {
+            alert("O link que você inseriu não é válido! :-(");
+        }
     }
 
     return (
         <div id="usuario">
-            <img src={userImage.default} alt="" />
+            <img src={userImage} alt="" onClick={changeUserImage} />
             <div id="texto">
                 <strong>catanacomics</strong>
                 <span>
