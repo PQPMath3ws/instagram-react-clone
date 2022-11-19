e925b160489ec521371b7913ea5ac9a3b775e774
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-19 11:29:03 -0300

    Adicionada mudança de nome de conta

--- src/components/User.js
+++ src/components/User.js
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import { Pencil } from 'react-ionicons'
 
 import "../assets/css/user.css";
 
 const userImage = require("../assets/images/catanacomics.svg");
 
-const User = () => (
-    <div id="usuario">
-        <img src={userImage.default} alt="" />
-        <div id="texto">
-            <strong>catanacomics</strong>
-            <span>
-                Catana
-                <Pencil cssClasses="ion-icon" color="#8E8E8E" width="14px" height="14px"></Pencil>
-            </span>
+const User = () => {
+    const [userName, setUsername] = useState("Catana");
+
+    function changeUsername() {
+        let name = prompt("Qual o seu novo (e belo) nome de usuário?");
+        setUsername(name ? name : userName);
+    }
+
+    return (
+        <div id="usuario">
+            <img src={userImage.default} alt="" />
+            <div id="texto">
+                <strong>catanacomics</strong>
+                <span>
+                    {userName}
+                    <Pencil cssClasses="ion-icon" color="#8E8E8E" width="14px" height="14px" onClick={changeUsername}></Pencil>
+                </span>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default User;
